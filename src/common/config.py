0db8d0ad3a026c8c0a6ca9bea766af89dffa7e01
LimitMax <taufiqqurohman98@gmail.com>
2026-02-05 14:22:49 +0700

    add stock assets

--- src/common/config.py
+++ src/common/config.py
@@ -1,21 +1,22 @@
-from typing import List
+from typing import Dict, List
 import os
 
 
-def load_active_assets() -> List[str]:
-    """
-    Load active asset symbols for the pipeline.
+def load_active_assets() -> List[Dict[str, str]]:
+     return [
+        # Crypto
+        {"symbol": "BTC-USD", "type": "crypto"},
+        {"symbol": "ETH-USD", "type": "crypto"},
+        {"symbol": "LTC-USD", "type": "crypto"},
+        {"symbol": "XRP-USD", "type": "crypto"},
+        {"symbol": "SOL-USD", "type": "crypto"},
 
-    Temporary implementation:
-    - Hardcoded list
-    - Can be replaced with YAML / DB later
-    """
-    return [
-        "BTC-USD",
-        "ETH-USD",
-        "LTC-USD",
-        "XRP-USD",
-        "SOL-USD",
+        # Stock
+        {"symbol": "AAPL", "type": "stock"},
+        {"symbol": "MSFT", "type": "stock"},
+        {"symbol": "GOOGL", "type": "stock"},
+        {"symbol": "AMZN", "type": "stock"},
+        {"symbol": "NVDA", "type": "stock"},
     ]
 
 
